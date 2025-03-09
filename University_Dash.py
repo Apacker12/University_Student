6e4a4749af90d8af8a52d01c9aa7d2ab3f53834d
Apacker12 <pachura@sunypoly.edu>
2025-03-09 00:06:35 -0500

    Update University_Dash.py

--- University_Dash.py
+++ University_Dash.py
@@ -5,3 +5,6 @@ import streamlit as st
 
 # Loading the data
 student_df = pd.read_excel("university_student_dashboard_data.xls")
+
+# Adding the title
+st.title(" :bar_chart: University Student Trend Analysis")
