a5520dae63f721dbe44b338e3011e3e65d21f93d
Apacker12 <pachura@sunypoly.edu>
2025-03-09 00:24:03 -0500

    Update University_Dash.py

--- University_Dash.py
+++ University_Dash.py
@@ -28,4 +28,34 @@ with col3:
   st.metric("Total Business Enrollees", f"{student_df['Business Enrolled'].sum():,.0f}")
   st.metric("Average Rentention", f"{average_retention:.2f}%")
 
+# Adding a new column with the text "08" for any Term of "Fall" and "01" for any Term of "Spring"
+student_df['Month'] = student_df['Term'].apply(lambda x: '08' if x == 'Fall' else ('01' if x == 'Spring' else ''))
 
+# Concatenating the Month and Term columns with an "/01/" in between the two
+student_df['Term_Date'] = student_df['Month'] + '/01/' + student_df['Year'].astype(str)
+
+# Changing the Term_Date column to a date type
+student_df['Term_Date'] = pd.to_datetime(student_df['Term_Date'])
+
+# Removing "Enrolled" from the column header
+student_df = student_df.rename(columns={'Engineering Enrolled': 'Engineering', 'Business Enrolled': 'Business', 'Arts Enrolled': 'Arts', 'Science Enrolled': 'Science'})
+
+st.sidebar.header("Choose your filter: ")
+
+# Creating a filter for the Year
+year = st.sidebar.multiselect("Select the Year", student_df["Year"].unique())
+if not year:
+    student_df2 = student_df.copy()
+else:
+    student_df2 = student_df[student_df["Year"].isin(year)]
+
+# Creating a filter for the Term
+term = st.sidebar.multiselect("Select the Term", student_df2["Term"].unique())
+if not term:
+    student_df3 = student_df2.copy()
+else:
+    student_df3 = student_df2[student_df2["Term"].isin(term)]
+
+# Data Table
+st.subheader("University Student Data")
+st.dataframe(student_df3)
