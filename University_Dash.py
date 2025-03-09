66e8d1e1e85a35c10ea9cfbf0e4635ca3251ebe6
Apacker12 <pachura@sunypoly.edu>
2025-03-09 00:06:58 -0500

    Update University_Dash.py

--- University_Dash.py
+++ University_Dash.py
@@ -8,3 +8,12 @@ student_df = pd.read_excel("university_student_dashboard_data.xls")
 
 # Adding the title
 st.title(" :bar_chart: University Student Trend Analysis")
+
+# Adding KPIs
+st.metric("Total Applications", f"${student_df['Applications'].sum():,.2f}")
+st.metric("Total Admissions", f"${student_df['Admitted'].sum():,.2f}")
+st.metric("Total Enrollments", f"${student_df['Enrolled'].sum():,.2f}")
+st.metric("Total Science Enrollees", {student_df['Science Enrolled'].sum()})
+st.metric("Total Engineering Enrollees", {student_df['Engineering Enrolled'].sum()})
+st.metric("Total Business Enrollees", {student_df['Business Enrolled'].sum()})
+st.metric("Total Arts Enrollees", {student_df['Arts Enrolled'].sum()})
