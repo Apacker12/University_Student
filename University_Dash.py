922e265d0337b53b4b13f6d75748f5a76aa6af44
Apacker12 <pachura@sunypoly.edu>
2025-03-09 01:01:04 -0500

    Update University_Dash.py

--- University_Dash.py
+++ University_Dash.py
@@ -44,7 +44,7 @@ average_retention = student_df3['Retention Rate (%)'].mean()
 admission_rate = (student_df3['Admitted'] / student_df3['Applications']) * 100
 average_admission_rate = admission_rate.mean()
 
-# Adding the KPIs
+# To add the first plot's KPIs
 col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.metric("Total Science Enrollees", f"{student_df3['Science'].sum():,.0f}")
@@ -67,7 +67,20 @@ plt.ylabel('Number of Enrollees')
 plt.legend()
 st.pyplot(fig1)
 
-#Creating a line plot for the retention, satisfaction, and admission rates over time
+# To add the second plot's KPIs
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("Average Admission Rate", f"{average_admission_rate:.2f}%")
+with col2:
+    st.metric("Average Satisfaction", f"{average_satisfaction:.2f}%")
+with col3:
+    st.metric("Average Retention", f"{average_retention:.2f}%")
+
+
+
+
+    
+# Creating a line plot for the retention, satisfaction, and admission rates over time
 fig2 = plt.figure(figsize=(12, 8))
 plt.plot(student_df3['Term_Date'], student_df3['Retention Rate (%)'], label='Retention Rate', color='magenta')
 plt.plot(student_df3['Term_Date'], student_df3['Student Satisfaction (%)'], label='Satisfaction', color='orange')
