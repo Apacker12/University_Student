c30afc0cefa5084be313510921da8f16fe88e621
Apacker12 <pachura@sunypoly.edu>
2025-03-09 00:26:12 -0500

    Update University_Dash.py

--- University_Dash.py
+++ University_Dash.py
@@ -1,4 +1,3 @@
-#import required libraries/packages
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -8,25 +7,6 @@ student_df = pd.read_excel("university_student_dashboard_data.xls")
 
 # Adding the title
 st.title(" :bar_chart: University Student Trend Analysis")
-
-# To calculate average satisfaction & retention rate
-average_satisfaction = student_df['Student Satisfaction (%)'].mean()
-average_retention = student_df['Retention Rate (%)'].mean()
-
-# Adding KPIs
-col1,col2, col3=st.columns(3)
-with col1:
-  st.metric("Total Applications", f"{student_df['Applications'].sum():,.0f}")
-  st.metric("Total Science Enrollees", f"{student_df['Science Enrolled'].sum():,.0f}")
-  st.metric("Total Arts Enrollees", f"{student_df['Arts Enrolled'].sum():,.0f}")
-with col2:
-  st.metric("Total Admissions", f"{student_df['Admitted'].sum():,.0f}")
-  st.metric("Total Engineering Enrollees", f"{student_df['Engineering Enrolled'].sum():,.0f}")
-  st.metric("Average Satisfaction", f"{average_satisfaction:.2f}%")
-with col3:
-  st.metric("Total Enrollments", f"{student_df['Enrolled'].sum():,.0f}")
-  st.metric("Total Business Enrollees", f"{student_df['Business Enrolled'].sum():,.0f}")
-  st.metric("Average Rentention", f"{average_retention:.2f}%")
 
 # Adding a new column with the text "08" for any Term of "Fall" and "01" for any Term of "Spring"
 student_df['Month'] = student_df['Term'].apply(lambda x: '08' if x == 'Fall' else ('01' if x == 'Spring' else ''))
@@ -56,6 +36,26 @@ if not term:
 else:
     student_df3 = student_df2[student_df2["Term"].isin(term)]
 
-# Data Table
+# Calculating average metrics using the filtered data
+average_satisfaction = student_df3['Student Satisfaction (%)'].mean()
+average_retention = student_df3['Retention Rate (%)'].mean()
+
+# Adding the KPIs
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("Total Applications", f"{student_df3['Applications'].sum():,.0f}")
+    st.metric("Total Science Enrollees", f"{student_df3['Science'].sum():,.0f}")
+    st.metric("Total Arts Enrollees", f"{student_df3['Arts'].sum():,.0f}")
+with col2:
+    st.metric("Total Admissions", f"{student_df3['Admitted'].sum():,.0f}")
+    st.metric("Total Engineering Enrollees", f"{student_df3['Engineering'].sum():,.0f}")
+    st.metric("Average Satisfaction", f"{average_satisfaction:.2f}%")
+with col3:
+    st.metric("Total Enrollments", f"{student_df3['Enrolled'].sum():,.0f}")
+    st.metric("Total Business Enrollees", f"{student_df3['Business'].sum():,.0f}")
+    st.metric("Average Retention", f"{average_retention:.2f}%")
+
+# Creating a data table
 st.subheader("University Student Data")
 st.dataframe(student_df3)
+
