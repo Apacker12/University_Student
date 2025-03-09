a3ee537e2f65e2e4994d8f78dd8acf5f986794fe
Apacker12 <pachura@sunypoly.edu>
2025-03-08 23:57:28 -0500

    Update University_Dash.py

--- University_Dash.py
+++ University_Dash.py
@@ -12,9 +12,10 @@ student_df = pd.read_excel("university_student_dashboard_data.xls")
 st.metric("Total Applications", f"${student_df['Applications'].sum():,.2f}")
 st.metric("Total Admissions", f"${student_df['Admitted'].sum():,.2f}")
 st.metric("Total Enrollments", f"${student_df['Enrolled'].sum():,.2f}")
-st.metric("Total Engineering Enrollees", {student_df['Engineering'].sum()})
-st.metric("Total Business Enrollees", {student_df['Business'].sum()})
-st.metric("Total Arts Enrollees", {student_df['Arts'].sum()})
+st.metric("Total Science Enrollees", {student_df['Science Enrolled'].sum()})
+st.metric("Total Engineering Enrollees", {student_df['Engineering Enrolled'].sum()})
+st.metric("Total Business Enrollees", {student_df['Business Enrolled'].sum()})
+st.metric("Total Arts Enrollees", {student_df['Arts Enrolled'].sum()})
 
 # Adding a new column with the text "08" for any Term of "Fall" and "01" for any Term of "Spring"
 student_df['Month'] = student_df['Term'].apply(lambda x: '08' if x == 'Fall' else ('01' if x == 'Spring' else ''))
